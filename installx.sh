f02cd27ab03a5f4ad28a3130924257965257f151
daycat <88031959+daycat@users.noreply.github.com>
2022-01-24 00:34:52 +0800

    FUCKKKKKKKK #4

--- installx.sh
+++ installx.sh
@@ -59,7 +59,7 @@ Install() {
 		echo -e " ${Tip} 安装主程序..."
 		mkdir /usr/local/PortForward
 		mkdir /usr/local/PortForward/slave
-		wget https://proxy.freecdn.workers.dev/?url=https%3A%2F%2Fgithub.com%2Fdaycat%2FwhmcsPortForward%2Fraw%2Fmain%2F%25E8%258A%2582%25E7%2582%25B9%25E5%2590%258E%25E7%25AB%25AF%2Fslave.zip -O slave.zip
+		wget 'https://proxy.freecdn.workers.dev/?url=https%3A%2F%2Fgithub.com%2Fdaycat%2FwhmcsPortForward%2Fraw%2Fmain%2F%25E8%258A%2582%25E7%2582%25B9%25E5%2590%258E%25E7%25AB%25AF%2Fslave.zip' -O slave.zip
 		unzip slave.zip -dir /usr/local/PortForward/slave
 		mv bconfig.php config.php
 		cd ..
